c36cabb79b83231e57df2d3ed7fb0e03dfb758ad
PaulCzaban <paul.czaban@googlemail.com>
2019-04-27 23:42:13 +0100

    Move conversion into model

--- blood.py
+++ blood.py
@@ -2,6 +2,6 @@ import nightscout_api.Api as ns
 
 api2 = ns.Api('http://czaban-nightscout.azurewebsites.net')
 
-blood_sugar = round(api2.get_sgvs()[0].sgv / 18, 2)
+blood_sugar = api2.get_sgvs()[0].sgv
 
 print(f'My blood sugar level is: {blood_sugar} mmol/L')
